7813634c59ac15af2d45c888dd41e21eb3fe5fc3
Samson <samson.seged@gmail.com>
2022-05-01 22:40:28 +0300

    changed the h1 to a list

--- info_site/index.js
+++ info_site/index.js
@@ -1,1 +1,6 @@
-ReactDOM.render(<h1>React Test</h1> , document.getElementById("root"))
+ReactDOM.render(
+    <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+    </ul>,
+    document.getElementById("root"))
