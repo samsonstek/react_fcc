46983701ee4d0e58cbbb2fe1ac07128d41897faa
Samson <samson.seged@gmail.com>
2022-05-02 10:39:49 +0300

    Created a navbar with a nav parent wrapper with h1  element and unordered list

--- info_site/index.js
+++ info_site/index.js
@@ -2,17 +2,28 @@
 //     return <h1>This is react</h1>
 // }
 
-// ReactDOM.render(
+// let page = (
 //     <div>
-//     <ul>
-//         <li>Thing 1</li>
-//         <li>Thing 2</li>
-//     </ul>
-//     <MainContent />
-//     </div>,
+//         <h1 className="header"> This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+// console.log(page)
+
+// ReactDOM.render( page
+//     ,
 //     document.getElementById("root"))
 
-let h1 = document.createElement("h1")
-h1.textContent = "This is an imperative way to program"
-h1.className = "header"
-document.getElementById("root").append(h1)
+let navbar = (
+    <nav>
+        <h1>Sami's Bistro</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
