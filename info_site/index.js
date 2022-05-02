b445ed0d2c07994a6326ed5d3b906a132fe6ed5a
Samson <samson.seged@gmail.com>
2022-05-02 09:54:48 +0300

    Imperative way of to program inorder to see the difference

--- info_site/index.js
+++ info_site/index.js
@@ -1,13 +1,18 @@
-function MainContent () {
-    return <h1>This is react</h1>
-}
+// function MainContent () {
+//     return <h1>This is react</h1>
+// }
 
-ReactDOM.render(
-    <div>
-    <ul>
-        <li>Thing 1</li>
-        <li>Thing 2</li>
-    </ul>
-    <MainContent />
-    </div>,
-    document.getElementById("root"))
+// ReactDOM.render(
+//     <div>
+//     <ul>
+//         <li>Thing 1</li>
+//         <li>Thing 2</li>
+//     </ul>
+//     <MainContent />
+//     </div>,
+//     document.getElementById("root"))
+
+let h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
